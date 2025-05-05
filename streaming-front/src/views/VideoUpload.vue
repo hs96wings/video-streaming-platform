5289adf7cfff30ae72d163f8118d682b58bd354f
hs96wings <hs96wings@gmail.com>
2025-05-05 15:10:09 +0900

    [Front]: 영상 업로드 구현

--- streaming-front/src/views/VideoUpload.vue
+++ streaming-front/src/views/VideoUpload.vue
@@ -5,7 +5,7 @@
                 <v-card>
                     <v-card-title class="text-h5 text-center">영상 추가</v-card-title>
                     <v-card-text>
-                        <v-form @submit.prevent="add">
+                        <v-form @submit.prevent="upload">
                             <v-text-field
                                 label="영상 제목"
                                 v-model="title"
@@ -13,10 +13,15 @@
                             </v-text-field>
                             <v-text-field
                                 label="영상 설명"
-                                v-model="describe"
+                                v-model="description"
                                 required>
                             </v-text-field>
-                            <v-btn type="submit" color="primary" block>추가</v-btn>
+                            <v-file-input
+                                label="영상 파일"
+                                @change="onFileChange">
+
+                            </v-file-input>
+                            <v-btn type="submit" color="primary" block>업로드</v-btn>
                         </v-form>
                     </v-card-text>
                 </v-card>
@@ -32,14 +37,23 @@ export default {
     data() {
         return {
             title: "",
-            videoId: "",
+            description: "",
+            file: null
         }
     },
     methods: {
-        async add() {
-            const videoData = { title: this.title, videoId: this.videoId }
-            await axios.post(`${process.env.VUE_APP_API_BASE_URL}/api/video/`, videoData)
-            window.location.href = '/video/list'
+        onFileChange(e) {
+            this.file = e.target.files[0]
+        },
+        async upload() {
+            const formData = new FormData()
+            formData.append("title", this.title)
+            formData.append("description", this.description)
+            formData.append("file", this.file)
+
+            await axios.post(`${process.env.VUE_APP_API_BASE_URL}/api/video/upload`, formData)
+            .then(() => window.location.href = "/list")
+            .catch(err => console.log(`업로드 실패: ${err.response.data}`))
         }
     }
 }
