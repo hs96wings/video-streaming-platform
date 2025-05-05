a06d1142c3a37d3511e9515920acbd9b6f13617a
hs96wings <hs96wings@gmail.com>
2025-05-06 08:55:29 +0900

    [Server-Test]: @Transactional이 붙어있지 않아 초기화되지 못하여 테스트 실패하는 현상 수정

--- streaming-server/src/test/java/io/github/hs96wings/streaming_server/member/integration/MemberServiceIntegrationTest.java
+++ streaming-server/src/test/java/io/github/hs96wings/streaming_server/member/integration/MemberServiceIntegrationTest.java
@@ -34,6 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Transactional
 public class MemberServiceIntegrationTest {
     @Autowired
     MockMvc mockMvc;
