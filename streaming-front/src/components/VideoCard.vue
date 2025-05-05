44d4ba9c222ad8cc2e94c8abb57ad3d4fd7a55a4
hs96wings <hs96wings@gmail.com>
2025-05-05 18:48:19 +0900

    [Front]: 영상 리스트 가져오기 구현

---
+++ streaming-front/src/components/VideoCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-card class="mx-auto" max-width="344">
+        <v-img
+            :src="video.thumbnailPath"
+            height="180"
+            class="white--text align-end"
+        ></v-img>
+        <v-card-title>{{ video.title }}</v-card-title>
+        <v-card-subtitle>{{ formattedDate }}</v-card-subtitle>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'videoCard',
+    props: {
+        video: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        formattedDate() {
+            const d = new Date(this.video.uploadedAt);
+            const yy = d.getFullYear();
+            const mm = String(d.getMonth() + 1).padStart(2, '0');
+            const dd = String(d.getDate()).padStart(2, '0');
+            const hh = String(d.getHours()).padStart(2, '0');
+            const mi = String(d.getMinutes()).padStart(2, '0');
+            return `${yy}.${mm}.${dd} ${hh}:${mi}`;
+        }
+    }
+}
+</script>
--- streaming-front/src/views/VideoList.vue
+++ streaming-front/src/views/VideoList.vue
@@ -1,8 +1,45 @@
 <template>
-    <v-container>
-
+    <v-container fluid>
+        <v-row dense>
+            <v-col
+                v-for="video in videoList"
+                :key="video.id"
+                cols="12" sm="5" md="4" lg="3"
+                @click="goToVideo(video.id)">
+                <VideoCard :video="video" />
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
 <script>
+import axios from 'axios'
+import VideoCard from '@/components/VideoCard.vue'
+
+export default {
+    name: 'VideoList',
+    components: { VideoCard },
+    props: {
+        isLogin: {
+            type: Boolean,
+            default: false,
+            required: true
+        },
+    },
+    data() {
+        return {
+            videoList: [],
+        }
+    },
+    async created() {
+        const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/api/video/list`)
+        console.log(response)
+        this.videoList = response.data;
+    },
+    methods: {
+        goToVideo(id) {
+            window.location.href="/video/" + id;
+        }
+    }
+}
 </script>
