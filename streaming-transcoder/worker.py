26871b93eaf7db3d9c60c8e228d269ab4af2cffd
hs96wings <hs96wings@gmail.com>
2025-05-08 07:26:41 +0900

    [Transcoder]: 영상 hls 처리, 썸네일 처리 구현

--- streaming-transcoder/worker.py
+++ streaming-transcoder/worker.py
@@ -1,14 +1,21 @@
 from redis import Redis
 import json, requests, subprocess
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 r = Redis(host='localhost', port=6379, db=0)
 
 def worker_loop():
     while True:
         _, job = r.blpop('videoQueue') # 블로킹 대기
         data = json.loads(job)
-        print(data)
         vid, path = data['videoId'], data['path']
         # 상태 업데이트: 호출 전 spring에 PATCH /api/video/{vid}/status?status=PROCESSING
         requests.patch(f"http://localhost:8080/api/video/{vid}/status?status=PROCESSING".format(vid))
-        # 여기까지 테스트
+        base_dir = os.environ.get("base_dir")
+        new_path = f"{base_dir}/hls_output/{vid}.m3u8"
+        subprocess.run(['ffmpeg', '-i', path, '-codec', 'copy', '-start_number', '0', '-hls_time', '10', '-hls_list_size', '0', '-f', 'hls', new_path])
+        thumb_path = f"{base_dir}/thumbs/{vid}.png"
+        subprocess.run(['ffmpeg', '-i', new_path, '-ss', '00:00:01', '-vframes', '1', thumb_path])
+        requests.patch(f"http://localhost:8080/api/video/{vid}/status?status=READY".format(vid))
