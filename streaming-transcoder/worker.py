172da0407cc6247101d5f66572720235dcc2ea4e
hs96wings <hs96wings@gmail.com>
2025-05-08 07:41:48 +0900

    [Total]: Server-flask가 영상 주소와 썸네일 주소를 보낼 수 있고 처리하도록 수정, Transcoder-물리적 위치에서 localhost 주소로 변경

--- streaming-transcoder/worker.py
+++ streaming-transcoder/worker.py
@@ -17,5 +17,11 @@ def worker_loop():
         new_path = f"{base_dir}/hls_output/{vid}.m3u8"
         subprocess.run(['ffmpeg', '-i', path, '-codec', 'copy', '-start_number', '0', '-hls_time', '10', '-hls_list_size', '0', '-f', 'hls', new_path])
         thumb_path = f"{base_dir}/thumbs/{vid}.png"
+
+        # 저장 위치는 물리적 위치가 맞는데 thumbnail_path와 video_path는 localhost:8080/uploads 폴더로 설정되어야 함
+        base_url = os.environ.get("base_url")
+        save_thumb_path = f"{base_url}/thumbs/{vid}.png"
+        save_video_path = f"{base_url}/hls_output/{vid}.m3u8"
         subprocess.run(['ffmpeg', '-i', new_path, '-ss', '00:00:01', '-vframes', '1', thumb_path])
-        requests.patch(f"http://localhost:8080/api/video/{vid}/status?status=READY".format(vid))
+        requests.patch(f"http://localhost:8080/api/video/{vid}/status?status=READY".format(vid),
+                       json={"videoPath": save_video_path, "thumbnailPath": save_thumb_path})
