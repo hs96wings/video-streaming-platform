c5085fbdc746407d06703338add2e06e8b02260b
hs96wings <hs96wings@gmail.com>
2025-05-10 12:57:15 +0900

    [Front]: 검색 시스템 구현

--- streaming-front/src/views/VideoList.vue
+++ streaming-front/src/views/VideoList.vue
@@ -1,5 +1,13 @@
 <template>
     <v-container fluid>
+        <!-- 검색 기능 -->
+        <v-text-field
+            v-model="searchKeyword"
+            label="영상 제목 검색"
+            @input="searchVideos"
+            clearable
+        >
+        </v-text-field>
         <v-row dense>
             <v-col
                 v-for="video in videoList"
@@ -29,6 +37,7 @@ export default {
     data() {
         return {
             videoList: [],
+            searchKeyword: "",
         }
     },
     async created() {
@@ -38,6 +47,20 @@ export default {
     methods: {
         goToVideo(id) {
             this.$router.push(`/video/${id}`);
+        },
+        async searchVideos() {
+            try {
+                if (this.searchKeyword.trim === "") {
+                    const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/api/video/list`);
+                    this.videoList = response.data;
+                } else {
+                    const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/api/video/search?title=${this.searchKeyword}`)
+                    this.videoList = response.data;
+                }
+            } catch (err) {
+                alert("검색 중 오류가 발생했습니다");
+                console.error(err);
+            }
         }
     }
 }
