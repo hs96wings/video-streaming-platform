e3a7080f1b3f991acdee2b3f64df23603bcf1572
hs96wings <hs96wings@gmail.com>
2025-06-16 15:06:41 +0900

    [Transcoder]: m3u8의 ts 파일이 중복되어 영상에 문제가 생기는 현상 수정

--- streaming-transcoder/worker.py
+++ streaming-transcoder/worker.py
@@ -28,11 +28,12 @@ def process_job(job):
     # 상태 업데이트: 호출 전 spring에 PATCH /api/video/{vid}/status?status=PROCESSING
     requests.patch(f"http://localhost:8080/api/video/{vid}/status?status=PROCESSING".format(vid))
     base_dir = os.environ.get("base_dir")
-    new_path = f"{base_dir}/hls_output/{vid}.m3u8"
+
+    output_dir = f"{base_dir}/hls_output/{vid}"
+    new_path = f"{output_dir}/index.m3u8"
     subprocess.run(['ffmpeg', '-i', path, '-codec', 'copy', '-start_number', '0', '-hls_time', '10', '-hls_list_size', '0', '-f', 'hls', new_path])
     thumb_path = f"{base_dir}/thumbs/{vid}.png"
 
-    # 저장 위치는 물리적 위치가 맞는데 thumbnail_path와 video_path는 localhost:8080/uploads 폴더로 설정되어야 함
     base_url = os.environ.get("base_url")
     save_thumb_path = f"{base_url}/thumbs/{vid}.png"
     save_video_path = f"{base_url}/hls_output/{vid}.m3u8"
