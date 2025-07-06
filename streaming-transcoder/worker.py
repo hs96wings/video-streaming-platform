6c4ed033b14a7c6616163b1f884180127d1cf565
hs96wings <hs96wings@gmail.com>
2025-07-07 05:16:53 +0900

    [Transcoder]: worker와 worker-prod 통합 관리

--- streaming-transcoder/worker.py
+++ streaming-transcoder/worker.py
@@ -5,7 +5,15 @@ import os
 import time
 
 load_dotenv()
-r = redis.Redis(host='localhost', port=6379, db=0)
+
+# 개발 환경 여부 (default: False)
+IS_DEV = os.getenv("IS_DEV", "false").lower() == "true"
+
+r = redis.Redis(
+    host=os.getenv("REDIS_HOST", "localhost" if IS_DEV else "redis"),
+    port=int(os.getenv("REDIS_PORT", 6379)),
+    db=0
+)
 
 def worker_loop():
     while True:
@@ -23,38 +31,41 @@ def worker_loop():
             time.sleep(2)
 
 def process_job(job):
+    SPRING_HOST = os.getenv("SPRING_HOST", "localhost" if IS_DEV else "streaming-server")
+    SPRING_PORT = os.getenv("SPRING_PORT", "8080")
 
-    login_url = "http://localhost:8080/api/auth/login"
+    login_url = f"http://{SPRING_HOST}:{SPRING_PORT}/api/auth/login"
+
     login_data = {
-        "userid": "admin",
-        "password": "1234"
+        "userid": os.getenv("ADMIN_ID", "admin"),
+        "password": os.getenv("ADMIN_PW", "1234" if IS_DEV else "admin7890")
     }
 
     res = requests.post(login_url, json=login_data)
     token = res.json().get("token")
-
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
+    headers = { "Authorization": f"Bearer {token}" }
 
     data = json.loads(job)
     vid, path = data['videoId'], data['path']
-    # 상태 업데이트: 호출 전 spring에 PATCH /api/video/{vid}/status?status=PROCESSING
-    requests.patch(f"http://localhost:8080/api/video/{vid}/status?status=PROCESSING".format(vid), headers=headers)
-    base_dir = os.environ.get("base_dir")
+    
+    # 상태 변경 요청 (PROCESSING)
+    requests.patch(f"http://{SPRING_HOST}:{SPRING_PORT}/api/video/{vid}/status?status=PROCESSING", headers=headers)
 
+    base_dir = os.environ.get("base_dir")
     output_dir = f"{base_dir}/hls_output/{vid}"
-    os.makedirs(f"{base_dir}/hls_output", exist_ok=True)
-    os.makedirs(f"{base_dir}/thumbs", exist_ok=True)
     os.makedirs(output_dir, exist_ok=True)
 
     new_path = f"{output_dir}/index.m3u8"
     subprocess.run(['ffmpeg', '-i', path, '-codec', 'copy', '-start_number', '0', '-hls_time', '10', '-hls_list_size', '0', '-f', 'hls', new_path])
     thumb_path = f"{base_dir}/thumbs/{vid}.png"
+    subprocess.run(['ffmpeg', '-i', new_path, '-ss', '00:00:01', '-vframes', '1', thumb_path])
 
-    base_url = os.environ.get("base_url")
+    base_url = os.environ.get("BASE_URL")
+    requests.patch(f"")
     save_thumb_path = f"{base_url}/thumbs/{vid}.png"
     save_video_path = f"{base_url}/hls_output/{vid}/index.m3u8"
-    subprocess.run(['ffmpeg', '-i', new_path, '-ss', '00:00:01', '-vframes', '1', thumb_path])
-    requests.patch(f"http://localhost:8080/api/video/{vid}/status?status=READY".format(vid),
-                    json={"videoPath": save_video_path, "thumbnailPath": save_thumb_path}, headers=headers)
+    
+    # 상태 변경 요청 (READY)
+    requests.patch(f"http://{SPRING_HOST}:{SPRING_PORT}/api/video/{vid}/status?status=READY",
+                    json={"videoPath": f"{base_url}/hls_output/{vid}/index.m3u8", "thumbnailPath": f"{base_url}/thumbs/{vid}.png"},
+                    headers=headers)
