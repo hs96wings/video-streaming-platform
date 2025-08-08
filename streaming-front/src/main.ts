322e382bbbff542263eefc256eb8b01d6971006e
hs96wings <hs96wings@gmail.com>
2025-08-09 04:19:00 +0900

    [Front]: TypeScript로 전환

--- streaming-front/src/main.ts
+++ streaming-front/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import App from './App.vue'
+import App from "./App.vue"
 import vuetify from './plugins/vuetify'
 import router from '@/router/index.js'
 import axios from 'axios'
