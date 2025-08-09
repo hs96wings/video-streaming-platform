34d197375f6535a3f07945d991446450c12a9ae1
hs96wings <hs96wings@gmail.com>
2025-08-10 02:16:45 +0900

    [Front]: VideoList.vue에 로딩 인디케이터 추가

--- streaming-front/src/views/VideoList.vue
+++ streaming-front/src/views/VideoList.vue
@@ -2,11 +2,18 @@
   <v-container fluid>
     <!-- 검색 기능 -->
     <v-text-field v-model="searchKeyword" label="영상 제목 검색" @input="searchVideos" clearable> </v-text-field>
-    <v-row dense>
+    <div v-if="isLoading" class="text-center pa-10">
+      <v-progress-circular indeterminate color="primary" :size="100"></v-progress-circular>
+      <p class="mt-4">영상을 불러오는 중입니다...</p>
+    </div>
+    <v-row dense v-else-if="videoList">
       <v-col v-for="video in videoList" :key="video.id" cols="12" sm="5" md="4" lg="3" @click="goToVideo(video.id)">
         <VideoCard :video="video" />
       </v-col>
     </v-row>
+    <div v-else class="text-center pa-10">
+      <p>영상을 불러오는 데 실패했습니다.</p>
+    </div>
   </v-container>
 </template>
 
@@ -20,10 +27,16 @@ import type { Video } from '@/types/api';
 const router = useRouter();
 const videoList = ref<Video[]>([]);
 const searchKeyword = ref<string>('');
+const isLoading = ref<boolean>(true);
 
 onMounted(async () => {
-  const data = await api.get(`/api/video/list`);
-  videoList.value = data;
+  isLoading.value = true;
+  try {
+    const data = await api.get(`/api/video/list`);
+    videoList.value = data;
+  } finally {
+    isLoading.value = false;
+  }
 });
 
 function goToVideo(id: number) {
